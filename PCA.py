Adaptive LR,fixed Normalization

PCA.py
@@ -6,16 +6,17 @@ from sklearn.datasets import load_iris
 import pickle
 from sklearn.model_selection import train_test_split
 
-
 # defines
-RawDataFile = 'PL_Data.mat'
-RawDataArrayName = 'data_pl_241105'  # mat文件中的数组名
+# RawDataFile = 'PL_Data.mat'
+# RawDataArrayName = 'data_pl_241105'  # mat文件中的数组名
+RawDataFile = 'PL_Data2.mat'
+RawDataArrayName = 'data_pl_241108'  # mat文件中的数组名
 
 RawData_UsedInput_Table = (  # 提取有用输入列,索引&注释 按原始数据顺序 下标从0开始
-    #[0, "纬度"],
-    #[1, "经度"],
+    [0, "纬度"],
+    [1, "经度"],
     [2, "频率"],
-    #[3, "仰角"],
+    [3, "仰角"],
     [4, "星地距离"],
     [5, "地面站高度"],
     [6, "地表温度"],
@@ -29,12 +30,12 @@ RawData_UsedInput_Table = (  # 提取有用输入列,索引&注释 按原始数�
 )
 
 RawData_UsedOutput_Table = (  # 提取有用输出列,索引&注释 按原始数据顺序 下标从0开始
-    [18, "总衰减"],
+    # [18, "总衰减"],
+    [19, "总衰减"],
 )
 
-RawData_UsedInput_Table = list(map(list, zip(*RawData_UsedInput_Table)))    # 转置
+RawData_UsedInput_Table = list(map(list, zip(*RawData_UsedInput_Table)))  # 转置
 RawData_UsedOutput_Table = list(map(list, zip(*RawData_UsedOutput_Table)))
-
 
 # 提取
 RawData = scio.loadmat(RawDataFile)  # dict 字典
@@ -42,33 +43,32 @@ RawArray = RawData[RawDataArrayName]
 
 UsedInputArray = RawArray[:, RawData_UsedInput_Table[0]]
 UsedOutputArray = RawArray[:, RawData_UsedOutput_Table[0]]
-#print(UsedInputArray)
-#print(UsedOutputArray)
+# print(UsedInputArray)
+# print(UsedOutputArray)
 
 # PCA
 ModelPCA = PCA(n_components=len(RawData_UsedInput_Table[0]))
 ModelPCA.fit(UsedInputArray)
 
 RawData_UsedInput_Table.append(ModelPCA.mean_.tolist())
-#print("PCA维数", ModelPCA.n_components_)
-#print("原始参量权重", RawData_UsedInput_Table[2])
+# print("PCA维数", ModelPCA.n_components_)
+# print("原始参量权重", RawData_UsedInput_Table[2])
 
 # 排序索引表和希望使用的输入数据
-RawData_UsedInput_Table = list(map(list, zip(*RawData_UsedInput_Table)))    # 转置
-Sorted_UsedInput_Table = sorted(RawData_UsedInput_Table, key=(lambda x: x[2]), reverse=True)    # 排序
-Sorted_UsedInput_Table = list(map(list, zip(*Sorted_UsedInput_Table)))    # 转置
+RawData_UsedInput_Table = list(map(list, zip(*RawData_UsedInput_Table)))  # 转置
+Sorted_UsedInput_Table = sorted(RawData_UsedInput_Table, key=(lambda x: x[2]), reverse=True)  # 排序
+Sorted_UsedInput_Table = list(map(list, zip(*Sorted_UsedInput_Table)))  # 转置
 Sorted_UsedInputArray = RawArray[:, Sorted_UsedInput_Table[0]]
 
 Sorted_UsedInput_Table.append(ModelPCA.explained_variance_ratio_.tolist())  # 方差和原始权重排序不相关 仅参考
 
-Sorted_UsedInput_Table = list(map(list, zip(*Sorted_UsedInput_Table)))    # 转置
+Sorted_UsedInput_Table = list(map(list, zip(*Sorted_UsedInput_Table)))  # 转置
 print("PCA排序的索引表", Sorted_UsedInput_Table)
 print("PCA排序的输入数据", Sorted_UsedInputArray)
 
-
 # 随机划分
 DataSet = np.hstack((Sorted_UsedInputArray, UsedOutputArray))
-train_set, test_set = train_test_split(DataSet, test_size=0.2, random_state=34)
+train_set, test_set = train_test_split(DataSet, test_size=0.05, random_state=78)
 
 # 保存排序的索引表 输入数据 标签数据
 train_x, train_y = np.hsplit(train_set, [-1])

Test.py
@@ -7,7 +7,7 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-InputNum = 6
+InputNum = 12
 
 # 提取预处理测试数据
 with open('DataSet_IndexTable.bin', 'rb') as file:
@@ -22,19 +22,25 @@ with open('Test_Y.bin', 'rb') as file:
     OutputArray = pickle.load(file)
     file.close()
 
+with open('x_normalization_factor.bin', 'rb') as file:
+    x_nor = pickle.load(file)
+    file.close()
+
+with open('y_normalization_factor.bin', 'rb') as file:
+    y_nor = pickle.load(file)
+    file.close()
+
+
 Net = torch.load('Net.pt')
 
 
 # 测试集 处理
-def normalization(data):
-    _range = data.max(axis=0) - data.min(axis=0)
-    return (data - data.min(axis=0)) / _range
+def apply_normalization(data, _min, _range):
+    return (data - _min) / _range
 
 
-def de_normalization(original, target):
-    _min = original.min(axis=0)
-    _range = original.max(axis=0) - _min
-    return target * _range + _min
+def apply_de_normalization(data, _min, _range):
+    return data * _range + _min
 
 
 def mape(true, pre):
@@ -44,8 +50,8 @@ def mape(true, pre):
 
 
 InputArray = InputArray[:, 0:InputNum]
-InputArray_Nor = normalization(InputArray)
-OutputArray_Nor = normalization(OutputArray)  # 归一化
+InputArray_Nor = apply_normalization(InputArray, x_nor[0], x_nor[1])
+OutputArray_Nor = apply_normalization(OutputArray, y_nor[0], y_nor[1])  # 归一化
 
 # Test 使用CUDA
 Net_cuda = Net.to(device)
@@ -54,7 +60,7 @@ InputArray_Nor_cuda = InputArray_Nor.to(device)
 
 Yp_Nor_cuda = Net(InputArray_Nor_cuda)
 Yp_Nor = Yp_Nor_cuda.cpu().detach().numpy()
-Yp = de_normalization(OutputArray, Yp_Nor)
+Yp = apply_de_normalization(Yp_Nor, y_nor[0], y_nor[1])
 
 loss_Nor = mape(Yp_Nor, OutputArray_Nor)
 loss = mape(Yp, OutputArray)
@@ -68,6 +74,9 @@ a = np.hstack((OutputArray, Yp)).tolist()
 a.sort(key=(lambda x: x[0]))
 a = list(map(list, zip(*a)))
 
+b = (OutputArray - Yp).tolist()
+
 plt.plot(np.arange(len(OutputArray)), a[1])
 plt.plot(np.arange(len(OutputArray)), a[0])
+plt.plot(np.arange(len(OutputArray)), b)
 plt.show(block=True)

Train.py
@@ -3,12 +3,14 @@ import numpy as np
 import torch
 from torch import nn
 import matplotlib.pyplot as plt
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# device = torch.device('cpu')
 
 # 输入参数数量
-InputNum = 6
-TrainNum = 20000
-LR = 2
+InputNum = 12
+TrainNum = 200000
+LR_Initial = 4
 
 # 提取预处理训练数据
 with open('DataSet_IndexTable.bin', 'rb') as file:
@@ -25,24 +27,39 @@ with open('Train_Y.bin', 'rb') as file:
 
 
 # 处理数据 创建网络
-def normalization(data):
-    _range = data.max(axis=0) - data.min(axis=0)
-    return (data - data.min(axis=0)) / _range
+def apply_normalization(data, _min, _range):
+    return (data - _min) / _range
+
+
+def get_normalization_factor(data):
+    _min = data.min(axis=0)
+    _range = data.max(axis=0) - _min
+    return _min, _range
 
 
 InputArray = InputArray[:, 0:InputNum]
-InputArray = normalization(InputArray)  # 归一化
-OutputArray = normalization(OutputArray)  # 归一化
+x_normalization_min, x_normalization_range = get_normalization_factor(InputArray)
+y_normalization_min, y_normalization_range = get_normalization_factor(OutputArray)
+InputArray = apply_normalization(InputArray, x_normalization_min, x_normalization_range)  # 归一化
+OutputArray = apply_normalization(OutputArray, y_normalization_min, y_normalization_range)  # 归一化
 
 InputArray = torch.tensor(InputArray, dtype=torch.float32)
 OutputArray = torch.tensor(OutputArray, dtype=torch.float32)
 
-Net = nn.Sequential(nn.Linear(InputNum, 50), nn.ReLU(),
-                    nn.Linear(50, 16), nn.ReLU(),
-                    nn.Linear(16, 1), nn.Softplus())
+Net = nn.Sequential(nn.Linear(InputNum, 64),    nn.LeakyReLU(),
+                    nn.Linear(64, 48),          nn.LeakyReLU(),
+                    nn.Linear(48, 24),          nn.LeakyReLU(),
+                    nn.Linear(24, 1),           nn.Softplus())
 
 Loss = nn.MSELoss()
-optim = torch.optim.SGD(params=Net.parameters(), lr=LR)  # 随机梯度下降
+optim = torch.optim.SGD(params=Net.parameters(), lr=LR_Initial)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optim,
+                                                       mode='min',
+                                                       factor=0.9995,
+                                                       threshold=1e-7,
+                                                       patience=0,
+                                                       cooldown=1,
+                                                       min_lr=0.1)
 
 # 训练 使用CUDA
 Net_cuda = Net.to(device)
@@ -56,13 +73,16 @@ for i in range(TrainNum):
     optim.zero_grad()
     loss.backward()  # 反向传递
     optim.step()  # 更新参数
+    scheduler.step(loss)
 
-    if i == TrainNum * 0.5:
-        optim = torch.optim.SGD(params=Net.parameters(), lr=LR * 0.5)
-
-    if i == TrainNum * 0.25:
-        optim = torch.optim.SGD(params=Net.parameters(), lr=LR * 0.25)
-
-    print(i, loss)
+    print(i, loss, optim.param_groups[0]['lr'])
 
 torch.save(Net_cuda, 'Net.pt')
+
+with open('x_normalization_factor.bin', 'wb') as file:
+    pickle.dump([x_normalization_min, x_normalization_range], file)
+    file.close()
+
+with open('y_normalization_factor.bin', 'wb') as file:
+    pickle.dump([y_normalization_min, y_normalization_range], file)
+    file.close()